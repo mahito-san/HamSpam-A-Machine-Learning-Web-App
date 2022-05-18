bec15947d4c14229c4d85ef590d8208f0ce04c8a
Arnob99 <t.k.arnob.999@gmail.com>
2022-05-18 22:49:47 +0600

    Added most critical things

---
+++ .ipynb_checkpoints/ProcessData-checkpoint.py
@@ -0,0 +1,53 @@
+import re
+import string
+import numpy as np
+import pandas as pd
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+    
+def clean_text(text):
+    '''Make text lowercase, 
+    remove text in square brackets, 
+    remove links, 
+    remove punctuation and 
+    remove words containing numbers.'''
+
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
+
+def remove_stopwords(text):
+    stop_words = stopwords.words('english')
+    text = ' '.join(word for word in text.split(' ') if word not in stop_words)
+    return text
+
+def stem_text(text):
+    stemmer = nltk.SnowballStemmer('english')
+    text = ' '.join(stemmer.stem(word) for word in text.split(' '))
+    return text
+
+def embed(corpus): 
+    return tokenizer.texts_to_sequences(corpus)
+
+def preprocess_data(text):
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    text = clean_text(text)
+    text = remove_stopwords(text)
+    text = stem_text(text)
+    text = pd.Series(text)
+    text = pad_sequences(
+        embed(text), 
+        101, 
+        padding='post'
+    )
+    return text
